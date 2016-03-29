3eb3e85c4dd327ac1a166d76b6f237e577fe5a22
andrehertwig <andre_hertwig@gmx.de>
2016-03-29 22:28:33 +0200

    Fix: servlet configuration

--- admin-tools-demo/src/main/java/de/chandre/admintool/JMiniXConfiguration.java
+++ admin-tools-demo/src/main/java/de/chandre/admintool/JMiniXConfiguration.java
@@ -4,11 +4,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jminix.console.servlet.MiniConsoleServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 /**
  * Servlet initializer for JMiniX (JMX) Servlet
@@ -21,6 +23,9 @@ public class JMiniXConfiguration extends SpringBootServletInitializer
 {
 	private static final Logger LOGGER = LogManager.getFormatterLogger(JMiniXConfiguration.class);
 	
+	@Autowired
+	private Environment environment;
+	
 	@Bean
     public MiniConsoleServlet jMiniXServlet() {
         return new MiniConsoleServlet();
@@ -29,6 +34,16 @@ public class JMiniXConfiguration extends SpringBootServletInitializer
 	@Bean
 	public ServletRegistrationBean jMiniXServletRegistration(@Value("${jminix.urlMappings:/jmx/*}") String urlMappings) {
 		ServletRegistrationBean registration = new ServletRegistrationBean(jMiniXServlet());
+		
+		if (null == urlMappings || urlMappings.startsWith("$")) {
+			if (null != environment && null != environment.getProperty("jminix.urlMappings")) {
+				urlMappings = environment.getProperty("jminix.urlMappings");
+			} else {
+				urlMappings = "/jmx/*";
+			}
+		}
+		
+		
 		LOGGER.info("initalize JMX-Servlet with URL mapping: %s", urlMappings);
 		for (String urlMapping : urlMappings.split(";")) {
 			if (StringUtils.isNotBlank(urlMapping)) {
