d6c8ab3842b3d7a834b48957961007d74d9750d5
andrehertwig <andre_hertwig@gmx.de>
2016-08-05 19:51:13 +0200

    Fix #1 resource loading ordered in wrong way (Tomcat)

--- admin-tools-core/src/main/java/de/chandre/admintool/core/thymeleaf/TemplateUrlComparator.java
+++ admin-tools-core/src/main/java/de/chandre/admintool/core/thymeleaf/TemplateUrlComparator.java
@@ -1,6 +1,10 @@
 package de.chandre.admintool.core.thymeleaf;
 
 import java.util.Comparator;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Comparator expects the full url path of template<br>
@@ -11,7 +15,12 @@ import java.util.Comparator;
  */
 public class TemplateUrlComparator implements Comparator<String> {
 	
-	private static final String[] CORE_LIB = new String[]{"/admin-tools-core/", "/admin-tools-core-"};
+	private static final Log LOGGER = LogFactory.getLog(TemplateUrlComparator.class);
+	
+	private static final Pattern[] CORE_LIB = new Pattern[]{ 
+			Pattern.compile(".*/admin-tools-core/.*"),
+			Pattern.compile(".*/admin-tools-core-\\d.*")
+	};
 	
 	private static int NEG = -1;
 	private static int POS = 1;
@@ -32,10 +41,12 @@ public class TemplateUrlComparator implements Comparator<String> {
 	
 	private boolean foundInCore(String url) {
 		boolean foundInCore = false;
-		for (String lib : CORE_LIB) {
-			foundInCore |= url.contains(lib);
+		for (Pattern libPattern : CORE_LIB) {
+			foundInCore |= libPattern.matcher(url).matches();
+		}
+		if (LOGGER.isTraceEnabled()) {
+			LOGGER.trace(String.format("template found in core %s for %s", foundInCore, url));
 		}
 		return foundInCore;
 	}
-
 }
